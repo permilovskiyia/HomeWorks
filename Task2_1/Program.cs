fe1d86cb0e1ff80873f546c8ab3f1d6749b9cb7d
permilovskiyia <permilovskiyia@pik.ru>
2025-02-19 16:38:23 +0300

    Загружаю 1 задания 2 темы

---
+++ Task2_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int d = c;
+            c = b; b = a; a = d;
+            Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, c);
+            Console.ReadKey();
+        }
+    }
+}
