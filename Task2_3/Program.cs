bf899155104ffffb94944e78bff133b0a7445d63
permilovskiyia <permilovskiyia@pik.ru>
2025-02-19 17:54:02 +0300

    Загрузил 3 задачу 2 темы

---
+++ Task2_3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task2_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double inch = Convert.ToDouble(Console.ReadLine());
+            double mm = inch * 25.4;
+            int m = (int)mm / 1000;
+            int sm = (int)mm % 1000 / 10;
+            mm %= 10;
+            Console.WriteLine("{0} m {1} sm {2} mm", m, sm, mm);
+            Console.ReadKey();
+        }
+    }
+}
