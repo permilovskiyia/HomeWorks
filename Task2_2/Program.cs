dd1ad8c12cb6bdc7c436f63b6e6c3c5ad3e3ce2d
permilovskiyia <permilovskiyia@pik.ru>
2025-02-19 16:58:27 +0300

    Загружаю 2 задание 2 темы

---
+++ Task2_2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double d = Convert.ToDouble(Console.ReadLine());
+            double m = Convert.ToDouble(Console.ReadLine());
+            double s = Convert.ToDouble(Console.ReadLine());
+            double rad = (d + m / 60 + s / 3600) * Math.PI / 180;
+            Console.WriteLine(rad);
+            Console.ReadKey();
+        }
+    }
+}
