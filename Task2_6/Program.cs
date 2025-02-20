812b3293fb867d532afd10f8e379156a1ada9104
permilovskiyia <permilovskiyia@pik.ru>
2025-02-20 12:46:30 +0300

    Задание 6 и 7 темы 2

---
+++ Task2_6/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Task2_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double l = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Math.Round(l*2)/2);
+        }
+    }
+}
---
+++ Task2_7/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task2_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            a += b * 10;
+            b = a % 10;
+            a = (int)a / 10;
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+        }
+    }
+}
