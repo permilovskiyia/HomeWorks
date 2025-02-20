fe83817dd27286a203511b9ff81202027a877ba6
permilovskiyia <permilovskiyia@pik.ru>
2025-02-20 12:13:50 +0300

    Задания 4, 5 темы 2

---
+++ Task2_4/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task2_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = (int)a / 100 % 10;
+            int c = a % 10;
+            Console.WriteLine(a - b * 99 + c * 99);
+        }
+    }
+}
---
+++ Task2_5/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Task2_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double l = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Math.Round(l, MidpointRounding.ToPositiveInfinity));
+        }
+    }
+}
